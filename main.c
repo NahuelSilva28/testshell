00644971b38d084d135260f4beb05a833d36b5e4
NahuelSilva28 <104049259+NahuelSilva28@users.noreply.github.com>
2023-04-26 09:21:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-    char *line;
-    char **args;
-    int status;
+    char *line; /* Pointer to the line read from stdin */
+    char **args;/* Array of arguments parsed from the line */
+    int status;/* Status of the executed command */
 
     do {
         printf(" :) shell > ");
-        line = read_line();
-        args = split_line(line);
-        status = execute(args);
+        line = read_line(); /* Read the line from stdin */
+        args = split_line(line);  /* Split the line into an array of arguments */
+        status = execute(args); /* Execute the command with its arguments */
 
-        free(line);
-        free(args);
+        free(line); /* Free the memory allocated for the line */
+        free(args); /* Free the memory allocated for the arguments */
 
-    } while (status);
+    } while (status); /* Continue the loop until the shell is exited */
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; /* exit */
 }
