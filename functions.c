7b74312b25a0e6b5732921ab397a76f0121d6c6a
NahuelSilva28 <104049259+NahuelSilva28@users.noreply.github.com>
2023-04-26 09:31:19 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -8,10 +8,10 @@
  */
 char *read_line(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0;
+    char *line = NULL;  /* initialize the pointer to the input line */
+    size_t bufsize = 0;  /* initialize the size of the buffer */
 
-    if (getline(&line, &bufsize, stdin) == -1) {
+    if (getline(&line, &bufsize, stdin) == -1) {  /*read the input line*/
         perror("getline");
         exit(EXIT_FAILURE);
     }
@@ -28,35 +28,35 @@ char *read_line(void)
 char **split_line(char *line)
 {
     int bufsize = BUFFER_SIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
+    char **tokens = malloc(bufsize * sizeof(char*)); /* allocate memory for an array of tokens */
     char *token, *saveptr;
 
-    if (!tokens) {
+    if (!tokens) { /* check if memory allocation was successful */
         fprintf(stderr, "Allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    token = strtok_r(line, TOKEN_DELIMITERS, &saveptr);
+    token = strtok_r(line, TOKEN_DELIMITERS, &saveptr); /* split the line into tokens */
     while (token != NULL) {
-        tokens[position] = strdup(token);
-        if (!tokens[position]) {
+        tokens[position] = strdup(token);  /* allocate memory for each token and store it in the array */
+        if (!tokens[position]) {  /* check memo */
             fprintf(stderr, "Allocation error\n");
             exit(EXIT_FAILURE);
         }
         position++;
 
-        if (position >= bufsize) {
+        if (position >= bufsize) { /* check if the buffer is full */
             bufsize += BUFFER_SIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
+            tokens = realloc(tokens, bufsize * sizeof(char*));  /* reallocate memory */
+            if (!tokens) {  /* check if memory allocation was successful */
                 fprintf(stderr, "Allocation error\n");
                 exit(EXIT_FAILURE);
             }
         }
 
-        token = strtok_r(NULL, TOKEN_DELIMITERS, &saveptr);
+        token = strtok_r(NULL, TOKEN_DELIMITERS, &saveptr); /* get the next token */
     }
-    tokens[position] = NULL;
+    tokens[position] = NULL; /* set the last element of the array to NULL */
     return tokens;
 }
 
